d6b97ac69e9a31d28be4e5190eb093d1363b474d
espen <espenbfo@stud.ntnu.no>
2023-09-25 13:54:43 +0200

    fix: classes

--- dataset.py
+++ dataset.py
@@ -6,8 +6,8 @@ from torch.utils.data import random_split
 def load_datasets(folder: Path, target_size=(224,224), train_fraction=0.8):
     dataset =  ImageFolder(folder, transform=Compose([RandomHorizontalFlip(), RandomResizedCrop(target_size, (0.8, 1.0)), ToTensor()]))
     if (train_fraction == 1):
-        return dataset, None
-    return random_split(dataset, (train_fraction, 1-train_fraction))
+        return dataset, None, dataset.classes
+    return random_split(dataset, (train_fraction, 1-train_fraction)), dataset.classes
 
 def load_dataloader(dataset, batch_size, shuffle=True):
     return DataLoader(dataset, batch_size, shuffle)
