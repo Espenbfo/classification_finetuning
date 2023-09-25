3957b28ef65e66fef48c0e8410f07a9fba6056c6
espen <espenbfo@stud.ntnu.no>
2023-09-25 13:50:34 +0200

    feat: clean up code

--- dataset.py
+++ dataset.py
@@ -4,9 +4,10 @@ from torchvision.datasets import ImageFolder
 from torchvision.transforms import *
 from torch.utils.data import random_split
 def load_datasets(folder: Path, target_size=(224,224), train_fraction=0.8):
+    dataset =  ImageFolder(folder, transform=Compose([RandomHorizontalFlip(), RandomResizedCrop(target_size, (0.8, 1.0)), ToTensor()]))
     if (train_fraction == 1):
-        return ImageFolder(folder, transform=Compose([RandomHorizontalFlip(), RandomResizedCrop(target_size, (0.8, 1.0)), ToTensor()])), None
-    return random_split(ImageFolder(folder, transform=Compose([RandomHorizontalFlip(), RandomResizedCrop(target_size, (0.8, 1.0)), ToTensor()])), (train_fraction, 1-train_fraction))
+        return dataset, None
+    return random_split(dataset, (train_fraction, 1-train_fraction))
 
 def load_dataloader(dataset, batch_size, shuffle=True):
     return DataLoader(dataset, batch_size, shuffle)
